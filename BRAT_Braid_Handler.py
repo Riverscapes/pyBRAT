a33a1e600b83838a554d98a6ae199d8643336dd7
banderson1618 <banderson1618@gmail.com>
2018-03-12 11:59:02 -0600

    Added the BraidedStream.py class added to the base structure of BRAT_Braid_Handler.py

--- BRAT_Braid_Handler.py
+++ BRAT_Braid_Handler.py
@@ -1,6 +1,40 @@
+# -------------------------------------------------------------------------------
+# Name:        BRAT Braid Handler
+# Purpose:     Handles the braid clusters in a stream network
+#
+# Author:      Braden Anderson
+#
+# Created:     03/2018
+# -------------------------------------------------------------------------------
+
+
 import arcpy
 from Cluster import Cluster
+from BraidStream import BraidStream
 
 
 def main(inputNetwork):
-    arcpy.AddMessage("Program Running")
+    """
+    The main function, where we go through the stream network and handle whatever we need to
+    :param inputNetwork: The stream network that we want to give mainstem values
+    :return: None
+    """
+    clusters = []
+
+    findClusters(inputNetwork, clusters)
+
+    handleClusters(inputNetwork, clusters)
+
+
+def findClusters(inputNetwork, clusters):
+    """
+    Where we build
+    :param inputNetwork:
+    :param clusters:
+    :return:
+    """
+    pass
+
+
+def handleClusters(inputNetwork, clusters):
+    pass
--- Cluster.py
+++ Cluster.py
@@ -1,8 +1,21 @@
-class Cluster:
-    def __init__(self, firstStream='', stream_array=[]):
-        self.streams = []
+# -------------------------------------------------------------------------------
+# Name:        Cluster
+# Purpose:     A class that holds a cluster of braided streams. Used primarily in BRAT_Braid_Handler.py
+#
+# Author:      Braden Anderson
+#
+# Created:     03/2018
+# -------------------------------------------------------------------------------
 
+class Cluster:
+    def __init__(self, firstStream=''):
+        """
+        The constructor. If 
+        :param firstStream:
+        """
+        self.streams = []
         if firstStream != '':
             self.streams.append(firstStream)
-        else:
-            self.streams=stream_array
+
+    def addStream(self, newStream):
+        self.streams.append(newStream)
