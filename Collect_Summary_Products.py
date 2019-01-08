06a701e527ae487523ed5f37bae84544b193e8db
banderson1618 <banderson1618@gmail.com>
2019-01-08 14:41:00 -0700

    Refactored Collect Summary Products for ease of maintainability #248

--- Collect_Summary_Products.py
+++ Collect_Summary_Products.py
@@ -13,19 +13,16 @@ from SupportingFunctions import make_folder
 import os
 
 def main(project_folder):
+    """
+    Our main function
+    :param project_folder: The BRAT Project that we want to collect the summary products for
+    :return:
+    """
     summary_prods_folder = make_folder(project_folder, "Summary_Products")
     ai_folder = make_folder(summary_prods_folder, "AI")
     png_folder = make_folder(summary_prods_folder, "PNG")
     pdf_folder = make_folder(summary_prods_folder, "PDF")
     kmz_folder = make_folder(summary_prods_folder, "KMZ")
-
-    png_output_folder = make_folder(png_folder, "Outputs")
-    png_inputs_folder = make_folder(png_folder, "Inputs")
-    png_intermediates_folder = make_folder(png_folder, "Intermediates")
-
-    pdf_output_folder = make_folder(pdf_folder, "Outputs")
-    pdf_inputs_folder = make_folder(pdf_folder, "Inputs")
-    pdf_intermediates_folder = make_folder(pdf_folder, "Intermediates")
 
     ai_files = []
     png_files = []
@@ -48,14 +45,24 @@ def main(project_folder):
 
     copy_all_files(ai_folder, ai_files)
     copy_all_files(kmz_folder, kmz_files)
-    copy_to_input_output_structure(png_folder, png_output_folder, png_inputs_folder, png_intermediates_folder, png_files)
-    copy_to_input_output_structure(pdf_folder, pdf_output_folder, pdf_inputs_folder, pdf_intermediates_folder, pdf_files)
+    copy_to_input_output_structure(png_folder, png_files)
+    copy_to_input_output_structure(pdf_folder, pdf_files)
 
 
-def copy_to_input_output_structure(folder_base, output_folder, input_folder, intermed_folder, files):
+def copy_to_input_output_structure(folder_base, files):
+    """
+    Copies our files into a "inputs, intermediates, outputs" folder structure
+    :param folder_base: The base folder that we want to copy our files into
+    :param files: A list of files that we want to copy
+    :return:
+    """
+    output_folder = make_folder(folder_base, "Outputs")
+    inputs_folder = make_folder(folder_base, "Inputs")
+    intermed_folder = make_folder(folder_base, "Intermediates")
+
     for file in files:
         if "\\Inputs\\" in file:
-            shutil.copy(file, input_folder)
+            shutil.copy(file, inputs_folder)
         elif "\\01_Intermediates\\" in file:
             shutil.copy(file, intermed_folder)
         elif "\\02_Analyses\\" in file:
