5436eb92060d9b50ae819bfd0ad85df7befb70ab
mhallerud <maggie.hallerud@aggiemail.usu.edu>
2019-02-11 17:40:55 -0700

    Adding Summary Products to BRAT folder structure

--- BRATProject.py
+++ BRATProject.py
@@ -31,6 +31,12 @@ def main(proj_path, proj_name, huc_ID, watershed_name, ex_veg, hist_veg, network
 
     inputs_folder = make_folder(proj_path, "Inputs")
 
+    # build summary products folder structure
+    summary_folder = make_folder(proj_path, "SummaryProducts")
+    add_to_summary_folder(summary_folder, 'PNG')
+    add_to_summary_folder(summary_folder, 'PDF')
+    add_to_summary_folder(summary_folder, 'AI')
+
     vegetation_folder = make_folder(inputs_folder, "01_Vegetation")
     network_folder = make_folder(inputs_folder, "02_Network")
     topo_folder = make_folder(inputs_folder, "03_Topography")
@@ -345,6 +351,18 @@ def add_metadata(new_xml_file, huc_ID, watershed_name):
     new_xml_file.add_sub_element(metadata_element, "Meta", huc_ID, [("name","HUCID")])
     new_xml_file.add_sub_element(metadata_element, "Meta", watershed_name, [("name","Watershed")])
 
+
+def add_to_summary_folder(summary_folder, sub_folder_name):
+    """
+    Builds folder structure for each output type within SummaryProducts directory
+    :param summary_folder: path to summary products folder for project
+    :param sub_folder_name: name of folder to be created in SummaryProducts directory
+    """
+    sub_folder = make_folder(summary_folder, sub_folder_name)
+    make_folder(sub_folder, "Inputs")
+    make_folder(sub_folder, "Intermediates")
+    make_folder(sub_folder, "Outputs")
+    
 
 if __name__ == '__main__':
     main(
