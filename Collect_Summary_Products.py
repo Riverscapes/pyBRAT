e3eb30e609c1128c7320a3147c7b6de24813ac8d
Brittany Graham <Brittany Graham@wheaton990.aggies.usu.edu>
2019-02-14 14:17:11 -0700

    Created a new Sheet and simpified code

--- Collect_Summary_Products.py
+++ Collect_Summary_Products.py
@@ -46,30 +46,140 @@ def write_capacity_sheets(workbook, stream_network, watershed_name):
     hist_vs_exist_worksheet = workbook.add_worksheet("Existing and Historic Capacity")
 
     write_exist_complex_worksheet(exist_complex_worksheet, stream_network, watershed_name, workbook)
+    write_exist_build_cap_worksheet(exist_build_cap_worksheet, stream_network, watershed_name, workbook)
 
 
-def write_exist_complex_worksheet(exist_complex_worksheet, stream_network, watershed_name, workbook):
-    write_header(exist_complex_worksheet, watershed_name)
-    KM_TO_MILES_RATIO = 0.6214
-
-    # Set the column size.
-    column_sizeA = exist_complex_worksheet.set_column('A:A', 30)
-    column_sizeB = exist_complex_worksheet.set_column('B:B', 20)
-    column_sizeC = exist_complex_worksheet.set_column('C:C', 20)
-
+# Writing the side headers for complex size
+def write_categories_complex(worksheet):
+    column_sizeA = worksheet.set_column('A:A', 30)
     row = 2
     col = 0
-    exist_complex_worksheet.write(row, col, "No Dams", column_sizeA)
+    worksheet.write(row, col, "No Dams", column_sizeA)
     row += 1
-    exist_complex_worksheet.write(row, col, "Single Dam")
+    worksheet.write(row, col, "Single Dam")
     row += 1
-    exist_complex_worksheet.write(row, col, "Small Complex (1-3 Dams)")
+    worksheet.write(row, col, "Small Complex (1-3 Dams)")
     row += 1
-    exist_complex_worksheet.write(row, col, "Medium Complex (3-5 dams)")
+    worksheet.write(row, col, "Medium Complex (3-5 dams)")
     row += 1
-    exist_complex_worksheet.write(row, col, "Large Complex (>5 dams)")
+    worksheet.write(row, col, "Large Complex (>5 dams)")
     row += 1
-    exist_complex_worksheet.write(row, col, "Total")
+    worksheet.write(row, col, "Total")
+
+
+# Writing the side headers for build capacity
+def write_categories_build_cap(worksheet):
+    column_sizeA = worksheet.set_column('A:A', 30)
+    row = 2
+    col = 0
+    worksheet.write(row, col, "None:0", column_sizeA)
+    row += 1
+    worksheet.write(row, col, "Rare: < 1")
+    row += 1
+    worksheet.write(row, col, "Occasional: 2 - 5")
+    row += 1
+    worksheet.write(row, col, "Frequent: 6 - 15 ")
+    row += 1
+    worksheet.write(row, col, "Pervasive: 16 - 40")
+    row += 1
+    worksheet.write(row, col, "Total")
+
+
+# Writing the data into the worksheet
+def write_data(data1, data2, data3, data4, data5, total_length, worksheet, workbook):
+    KM_TO_MILES_RATIO = 0.6214
+    # Set the column size.
+    column_sizeB = worksheet.set_column('B:B', 20)
+    column_sizeC = worksheet.set_column('C:C', 20)
+    # Adds the percent sign and puts it in percent form.
+    percent_format = workbook.add_format({'num_format': '0.00%'})
+    percent = worksheet.set_column('D:D', 10, percent_format)
+
+    col = 1
+    row = 2
+    worksheet.write(row, col, data1, column_sizeB)
+    col += 1
+    worksheet.write(row, col, data1 * KM_TO_MILES_RATIO, column_sizeC)
+    col += 1
+    worksheet.write(row, col, data1 / total_length, percent)
+
+    col = 1
+    row = 3
+    worksheet.write(row, col, data2)
+    col += 1
+    worksheet.write(row, col, data2 * KM_TO_MILES_RATIO)
+    col += 1
+    worksheet.write(row, col, data2 / total_length, percent)
+
+    col = 1
+    row = 4
+    worksheet.write(row, col, data3)
+    col += 1
+    worksheet.write(row, col, data3 * KM_TO_MILES_RATIO)
+    col += 1
+    worksheet.write(row, col, data3 / total_length, percent)
+
+    col = 1
+    row = 5
+    worksheet.write(row, col, data4)
+    col += 1
+    worksheet.write(row, col, data4 * KM_TO_MILES_RATIO)
+    col += 1
+    worksheet.write(row, col, data4 / total_length, percent)
+
+    col = 1
+    row = 6
+    worksheet.write(row, col, data5)
+    col += 1
+    worksheet.write(row, col, data5 * KM_TO_MILES_RATIO)
+    col += 1
+    worksheet.write(row, col, data5 / total_length, percent)
+
+    # Calculating Total for Stream Length(Km)
+    worksheet.write(7, 1, '=SUM(B3:B7)')
+    # Calculating Total for Stream Length (mi)
+    worksheet.write(7, 2, '=SUM(C3:C7)')
+    # Calculating total percentage.
+    worksheet.write(7, 3, '=SUM(D3:D7)', percent)
+
+
+# Getting the data for Complex Size
+def search_cursor(fields, data1, data2, data3, data4, data5, total, stream_network, is_complex, worksheet, workbook):
+    if is_complex:
+        with arcpy.da.SearchCursor(stream_network, fields) as cursor:
+            for length, ex_dam_complex_size in cursor:
+                total += length
+                if ex_dam_complex_size == 0:
+                    data1 += length
+                elif ex_dam_complex_size <= 1:
+                    data2 += length
+                elif ex_dam_complex_size <= 3:
+                    data3 += length
+                elif ex_dam_complex_size <= 5:
+                    data4 += length
+                else:
+                    data5 += length
+    else:
+        with arcpy.da.SearchCursor(stream_network, fields) as cursor:
+            for length, ex_build_cap_size in cursor:
+                total += length
+                if ex_build_cap_size == 0:
+                    data1 += length
+                elif ex_build_cap_size <= 1:
+                    data2 += length
+                elif ex_build_cap_size <= 5:
+                    data3 += length
+                elif ex_build_cap_size <= 15:
+                    data4 += length
+                elif ex_build_cap_size <= 40:
+                    data5 += length
+    write_data(data1, data2, data3, data4, data5, total, worksheet, workbook)
+
+def write_exist_complex_worksheet(exist_complex_worksheet, stream_network, watershed_name, workbook):
+    is_complex = True
+    write_header(exist_complex_worksheet, watershed_name)
+
+    write_categories_complex(exist_complex_worksheet)
 
     fields = ['SHAPE@Length', "mCC_EX_CT"]
     no_dams_length = 0.0
@@ -79,76 +189,43 @@ def write_exist_complex_worksheet(exist_complex_worksheet, stream_network, water
     many_dams_length = 0.0
     total_length = 0.0
 
-    with arcpy.da.SearchCursor(stream_network, fields) as cursor:
-        for length, ex_dam_complex_size in cursor:
-            total_length += length
-            if ex_dam_complex_size == 0:
-                no_dams_length += length
-            elif ex_dam_complex_size <= 1:
-                one_dam_length += length
-            elif ex_dam_complex_size <= 3:
-                some_dams_length += length
-            elif ex_dam_complex_size <= 5:
-                more_dams_length += length
-            else:
-                many_dams_length += length
+    search_cursor(fields, no_dams_length, one_dam_length, some_dams_length, more_dams_length, many_dams_length, total_length, stream_network, is_complex, exist_complex_worksheet, workbook)
 
-    # Adds the percent sign and puts it in percent form.
-    percent_format = workbook.add_format({'num_format': '0.00%'})
-    percent = exist_complex_worksheet.set_column('D:D', 10, percent_format)
+    # Writing the chart
 
-    # no_dams_length
-    col = 1
-    row = 2
-    # no_dams_length cannot be a string. Won't be able to calculate totals.
-    exist_complex_worksheet.write(row, col, no_dams_length, column_sizeB)
-    col += 1
-    exist_complex_worksheet.write(row, col, no_dams_length * KM_TO_MILES_RATIO, column_sizeC)
-    col += 1
-    exist_complex_worksheet.write(row, col, no_dams_length / total_length, percent)
+    # chart = workbook.add_chart({'type': 'bar'})
+    # chart.add_series({
+    #    'name': '=Sheet1!$A$1',
+    #    'categories': '=Sheet1!$A$3:$A$7',
+    #    'values': '=Sheet1!$C$3:$C$7',
+    # })
 
-    # one_dam_length
-    col = 1
-    row = 3
-    exist_complex_worksheet.write(row, col, one_dam_length)
-    col += 1
-    exist_complex_worksheet.write(row, col, one_dam_length * KM_TO_MILES_RATIO)
-    col += 1
-    exist_complex_worksheet.write(row, col, one_dam_length / total_length, percent)
+    # chart.add_series({
+    #    'name': ['Sheet1', 0, 2],
+    #   'categories': ['Sheet1', 1, 0, 6, 0],
+    #    'values': ['Sheet1', 1, 2, 6, 2],
+    # })
 
-    # some_dams_length
-    col = 1
-    row = 4
-    exist_complex_worksheet.write(row, col, some_dams_length)
-    col += 1
-    exist_complex_worksheet.write(row, col, some_dams_length * KM_TO_MILES_RATIO)
-    col += 1
-    exist_complex_worksheet.write(row, col, some_dams_length / total_length, percent)
+    # chart.set_title({'name': '=Sheet!$A$1'})
+    # exist_complex_worksheet.insert_chart('G3', chart)
 
-    # more_dams_length
-    col = 1
-    row = 5
-    exist_complex_worksheet.write(row, col, more_dams_length)
-    col += 1
-    exist_complex_worksheet.write(row, col, more_dams_length * KM_TO_MILES_RATIO)
-    col += 1
-    exist_complex_worksheet.write(row, col, more_dams_length / total_length, percent)
 
-    # many_dams_length
-    col = 1
-    row = 6
-    exist_complex_worksheet.write(row, col, many_dams_length)
-    col += 1
-    exist_complex_worksheet.write(row, col, many_dams_length * KM_TO_MILES_RATIO)
-    col += 1
-    exist_complex_worksheet.write(row, col, many_dams_length / total_length, percent)
+def write_exist_build_cap_worksheet(exist_build_cap_worksheet, stream_network, watershed_name, workbook):
+    is_complex = False
+    write_header(exist_build_cap_worksheet, watershed_name)
 
-    # Calculating Total for Stream Length(Km)
-    exist_complex_worksheet.write(7, 1, '=SUM(B3:B7)')
-    # Calculating Total for Stream Length (mi)
-    exist_complex_worksheet.write(7, 2, '=SUM(C3:C7)')
-    # Calculating total percentage. (Unsure if we need to add up all the percentages)
-    exist_complex_worksheet.write(7, 3, '=SUM(D3:D7)', percent)
+    write_categories_build_cap(exist_build_cap_worksheet)
+
+    fields = ['SHAPE@Length', "oCC_EX"]
+    none = 0.0
+    rare = 0.0
+    occasional = 0.0
+    frequent = 0.0
+    pervasive = 0.0
+    total_length = 0.0
+
+    search_cursor(fields, none, rare, occasional, frequent, pervasive, total_length, stream_network, is_complex, exist_build_cap_worksheet, workbook)
+
 
 
 def write_header(worksheet, watershed_name):
