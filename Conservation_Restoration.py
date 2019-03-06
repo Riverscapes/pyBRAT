7a1ce6d4debfdbf768c414fe5a2203229cfad930
banderson1618 <banderson1618@gmail.com>
2019-03-06 14:57:34 -0700

    One last iteration, hopefully

--- Conservation_Restoration.py
+++ Conservation_Restoration.py
@@ -167,9 +167,9 @@ def main(projPath, in_network, out_name):
             iPC_HighLU = row[10]
             infrastructure_dist = row[11]
             landuse = row[12]
-
             stream_power = row[14]
 
+            veg_departure = hist_dams - curr_dams
             urban = landuse > 0.66
             no_urban = not urban
             hist_to_curr_dams_ratio = 9999
@@ -182,21 +182,27 @@ def main(projPath, in_network, out_name):
             if hist_dams >= 15 and curr_dams >= 15 and no_urban and infrastructure_dist > 100:
                 row[15] = "Relocation and Conservation"
             elif hist_to_curr_dams_ratio > 1:
-                if hist_dams >= 5 and curr_dams > 1 and urban:
-                    row[15] = "Living with Beaver Solutions"
-                elif no_urban:
-                    if 5 <= hist_dams < 15 and 5 <= curr_dams < 15:
-                        row[15] = "High Restoration Potential"
-                    elif 5 <= hist_dams < 15 and 1 <= curr_dams < 5:
-                        if hist_veg >= 5:
-                            row[15] = "Medium Restoration Potential - Restore Veg First"
+                if hist_dams >= 5:
+                    if curr_dams > 1 and urban:
+                        row[15] = "Living with Beaver Solutions"
+                    elif 5 <= curr_dams < 15 and no_urban:
+                        if veg_departure >= 5:
+                            row[15] = "High Restoration Potential - Veg First"
+                        else:
+                            row[15] = "High Restoration Potential"
+                    elif 1 <= curr_dams < 5 and no_urban:
+                        if veg_departure >= 5:
+                            row[15] = "Medium Restoration Potential - Veg First"
                         else:
                             row[15] = "Medium Restoration Potential"
-                    elif 1 <= hist_dams < 5 and 1 <= curr_dams < 5:
-                        if hist_veg >= 5:
-                            row[15] = "Low Restoration Potential - Restore Veg First"
-                        else:
-                            row[15] = "Low Restoration Potential - Restore Veg First"
+                elif 1 <= hist_dams < 5 and 1 <= curr_dams < 5 and no_urban:
+                    if veg_departure >= 5:
+                        row[15] = "Low Restoration Potential - Veg First"
+                    else:
+                        row[15] = "Low Restoration Potential"
+
+
+
 
             cursor.updateRow(row)
 
