8eda6ff38390ab183025984b1074ae00a8611fb2
WHTNLAB-6MCHHK2\SaraBangen <sara.bangen@gmail.com>
2019-05-03 11:38:08 -0700

    Adding check for input flowline network.  If doesn't exist will print warning and pass to next directory.

--- SupportingTools/BatchScripts/BatchInputPrep/11_ExtractCanals_Batch.py
+++ SupportingTools/BatchScripts/BatchInputPrep/11_ExtractCanals_Batch.py
@@ -45,26 +45,29 @@ def main(overwrite = True):
 
         # extract canals and ditches for all watersheds if none present
         if not os.path.exists(canal_shp) or overwrite is True:
-            print "Extracting canal shapefile for " + dir
-            try:
-                tmp_huc8_flowline = arcpy.MakeFeatureLayer_management(flowline_shp, 'tmp_huc8_flowlines_lyr')
-                # select all flowlines with canal/ditch ftype (336) or that have 'Ditch' in name field
-                quer = """ "FTYPE" = 336 OR "GNIS_NAME" LIKE '%Ditch%' """
-                arcpy.SelectLayerByAttribute_management(tmp_huc8_flowline, 'NEW_SELECTION', quer)
-                # note: removes any pipelines with name 'Ditch' since these are excluded from segmented network
-                #       it's unlikely such cases occur, but just covering such instances
-                quer_remove = """ "FTYPE" = 428 OR "FTYPE" = 420 OR "FTYPE" = 566"""
-                arcpy.SelectLayerByAttribute_management(tmp_huc8_flowline, 'REMOVE_FROM_SELECTION', quer_remove)
-                # save output if any flowlines were selected
-                count = arcpy.GetCount_management(tmp_huc8_flowline)
-                ct = int(count.getOutput(0))
-                if ct >= 1:
-                    arcpy.CopyFeatures_management(tmp_huc8_flowline, canal_shp)
-            # catch errors and move to the next huc8 folder
-            except Exception as err:
-                print "Error with " + dir + ". Exception thrown was: "
-                print err
-
+            if os.path.exists(flowline_shp):
+                print "Extracting canal shapefile for " + dir
+                try:
+                    tmp_huc8_flowline = arcpy.MakeFeatureLayer_management(flowline_shp, 'tmp_huc8_flowlines_lyr')
+                    # select all flowlines with canal/ditch ftype (336) or that have 'Ditch' in name field
+                    quer = """ "FTYPE" = 336 OR "GNIS_NAME" LIKE '%Ditch%' """
+                    arcpy.SelectLayerByAttribute_management(tmp_huc8_flowline, 'NEW_SELECTION', quer)
+                    # note: removes any pipelines with name 'Ditch' since these are excluded from segmented network
+                    #       it's unlikely such cases occur, but just covering such instances
+                    quer_remove = """ "FTYPE" = 428 OR "FTYPE" = 420 OR "FTYPE" = 566"""
+                    arcpy.SelectLayerByAttribute_management(tmp_huc8_flowline, 'REMOVE_FROM_SELECTION', quer_remove)
+                    # save output if any flowlines were selected
+                    count = arcpy.GetCount_management(tmp_huc8_flowline)
+                    ct = int(count.getOutput(0))
+                    if ct >= 1:
+                        arcpy.CopyFeatures_management(tmp_huc8_flowline, canal_shp)
+                # catch errors and move to the next huc8 folder
+                except Exception as err:
+                    print "Error with " + dir + ". Exception thrown was: "
+                    print err
+            else:
+                print "WARNING: Canals cannot be extracted.  Input NHD flowline network does not exist for " + dir
+                pass
 
 if __name__ == "__main__":
     main()
